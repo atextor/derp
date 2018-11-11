25606d50f7a29f8d522db6628b81fe6c83e7dee3
Andreas Textor <textor.andreas@googlemail.com>
2018-11-11 12:37:35 +0100

    Add infrastructure for CUnit test suite to build

---
+++ test/testsuite.c
@@ -0,0 +1,9 @@
+#include "CUnitCI.h"
+
+/* Test that one equals one */
+static void test_simple_pass1(void) {
+    CU_ASSERT_FATAL(1 == 1);
+}
+
+CUNIT_CI_RUN("my-suite",
+             CUNIT_CI_TEST(test_simple_pass1));
